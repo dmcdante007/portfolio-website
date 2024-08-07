9ae8dc48a606d3b11a1646b4022a22d5720a350e
dmcdante007 <meshramswaraj98>
2024-08-07 12:22:14 +0530

    "added new Project"

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,7 @@ import ProjectItem from "./ProjectItem";
 import expImg from "../assets/Expense-tracker.jpg";
 import foodImg from "../assets/Real-food.jpg";
 import sizmuImg from "../assets/sizmu.jpg";
+import admin from "../assets/admin-dash.JPG"
 
 const Projects = () => {
   return (
@@ -31,6 +32,12 @@ const Projects = () => {
           img={sizmuImg}
           title="Music App"
           language="HTML CSS JS"
+          gitLink="https://github.com/dmcdante007/Sicmu--Responsive-music-website"
+        ></ProjectItem>
+        <ProjectItem
+          img={admin}
+          title="Admin-Dashboard"
+          language="HTML CSS JS ReactJS MUI Formik FullCalendar Data Grid"
           gitLink="https://github.com/dmcdante007/Sicmu--Responsive-music-website"
         ></ProjectItem>
       </div>
